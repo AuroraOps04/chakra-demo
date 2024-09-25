75e15c5826ca9f75fc2351087ed58bbd435ca11d
AuroraOps04 <5239604@qq.com>
2024-09-25 09:55:15 +0800

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Box, useColorModeValue } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <Box bg="gray.100" flex={1} minH="100vh" color={'tomato'}>
+      This is Vite Chakra UI
+    </Box>
+  )
+}
+
+export default App
